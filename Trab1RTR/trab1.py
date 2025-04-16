5c7f16dce9191719774e497e928f8ddc04f75b03
BudaKin <igorcrashtom@hotmail.com>
2025-04-16 09:25:15 -0300

    erro encontrado na lógica (resolver depois)

--- Trab1RTR/trab1.py
+++ Trab1RTR/trab1.py
@@ -9,23 +9,23 @@ def leitura(texto: str):
         # Verificando se a sequência foi encontrada
         if indice != -1 and indice + 257 < len(texto):
             if texto[indice+257] == "1":
-                substring = texto[indice:]
-                substring = ' '.join(substring)
+                res = texto[indice:]
                 print(f"foi encontrado a primeira flag: {indice}")
-                return substring
-            indice = indice+8
+                return res
             print(f"flag errada em: {indice}")
+            indice = indice+8
         else:
             print(f"não foi encontrada.")
             return "Erro"
 
 # Abrir o arquivo em modo escrita
 with open("saida.txt", "w") as saida:
-    with open("RX(vetor)MQ_v2.txt", "r") as entrada:
-    # with open("exercicio_dado.txt", "r") as entrada:
+    # with open("RX(vetor)MQ_v2.txt", "r") as entrada:
+    with open("exercicio_dado.txt", "r") as entrada:
         texto = entrada.read()
         substring = ''.join([c for c in texto if c in '01'])
         lido = leitura(substring)
+        lido = ' '.join(lido)
         lido = quebrar_linha(lido, 128)
         saida.write(lido)
             

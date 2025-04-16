5ae5552418da14408cc864719570ae15e2ab273c
BudaKin <igorcrashtom@hotmail.com>
2025-04-16 10:08:33 -0300

    Trabalho 1 finalizado

--- Trab1RTR/trab1.py
+++ Trab1RTR/trab1.py
@@ -7,21 +7,25 @@ def leitura(texto: str):
     while 1:
         indice = texto.find(PAQ, indice)
         # Verificando se a sequência foi encontrada
-        if indice != -1 and indice + 257 < len(texto):
+        if indice != -1 and len(texto) > 512 + indice:
             if texto[indice+257] == "1":
-                res = texto[indice:]
-                print(f"foi encontrado a primeira flag: {indice}")
-                return res
+                if (texto.find(PAQ, indice+8) == 512 + indice):
+                    substring = texto[indice:]
+                    print(f"foi encontrado a primeira flag: {indice}")
+                    return substring
             print(f"flag errada em: {indice}")
             indice = indice+8
         else:
             print(f"não foi encontrada.")
             return "Erro"
 
+
 # Abrir o arquivo em modo escrita
+
+arquivo = "RX(vetor)MQ_v2.txt"
+
 with open("saida.txt", "w") as saida:
-    # with open("RX(vetor)MQ_v2.txt", "r") as entrada:
-    with open("exercicio_dado.txt", "r") as entrada:
+    with open(arquivo, "r") as entrada:
         texto = entrada.read()
         substring = ''.join([c for c in texto if c in '01'])
         lido = leitura(substring)
